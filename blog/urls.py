8dce7d0512ba369bb86fb9d10007e0b6be0e6b25
Aashif Khan <oaashifo@gmail.com>
2017-11-15 13:16:43 +0530

    carrot on profile

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,7 @@ app_name = 'blogg'
 
 urlpatterns = [
 	url(r'^signup/$', views.signup, name='signup'),
-	url(r'profile/$', views.profile, name='profile'),
+	url(r'^profile/$', views.profile, name='profile'),
 	url(r'^$', views.post_list, name = 'post_list'),
 	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name = 'post_detail'),
 	url(r'^post/new/$', views.post_new, name = 'post_new'),
