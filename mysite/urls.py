ec4d1b0f4d2ce848c34da15120a74552e4a0c853
Aashif Khan <oaashifo@gmail.com>
2017-11-15 13:14:11 +0530

    Finally final

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,9 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('blog.urls')),
     url(r'^login/$', auth_views.login, name='login'),
-    #url(r'^login/$', auth_views.login, {'next_page': '/profile'}, name='login'),
-    #url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^logout/$', auth_views.logout, {'next_page': 'blogg:profile'}, name='logout'),
+    url(r'', include('blog.urls')),
 ]
