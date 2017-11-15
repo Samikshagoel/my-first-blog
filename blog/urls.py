b29bf15b6101e7d1bf079dbdd76143b4e9127408
Aashif Khan <oaashifo@gmail.com>
2017-11-15 13:26:19 +0530

    hello

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import url
 from . import views
-from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
 
 app_name = 'blogg'
 
 urlpatterns = [
-	url(r'^signup/$', views.signup, name='signup'),
+	url(r'^signup$', views.signup, name='signup'),
 	url(r'^profile/$', views.profile, name='profile'),
 	url(r'^$', views.post_list, name = 'post_list'),
 	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name = 'post_detail'),
